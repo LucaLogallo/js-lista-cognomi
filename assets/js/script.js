5af6e47487ffdb5da789410d42e5a8354ef352a8
LucaLogallo <lucalogallo1998@gmail.com>
2021-04-12 19:51:53 +0200

    aggiunta soluzione trovata su internet molto pi] corto e compreso

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,6 @@
   l = variabile di lavoro per lo scorrimento ed il salvataggio temporaneo dei valori in posizione i del vettore arr
   contenuto_precedente = variabile utilizzata per la stampa in una ol usata per concatenare le stringhe
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* 
  PROCEDIMENTO
  -inizializzo array arr vuoto
@@ -77,9 +64,32 @@ for(i=0;i<j;i++){
   var l = arr[i];
   if(l === cognomeU){
     var contenuto_precendente = document.getElementById("cognomi").innerHTML;
-    document.getElementById("cognomi").innerHTML = contenuto_precendente +"<li> il cognome utente che hai inserito: "+l+" si trova in posizione"+(i+1)+"</li>"; 
+    document.getElementById("cognomi").innerHTML = contenuto_precendente +"<li> il cognome utente che hai inserito: "+l+" si trova in posizione "+(i+1)+"</li>"; 
   }else{
     var contenuto_precendente = document.getElementById("cognomi").innerHTML;
     document.getElementById("cognomi").innerHTML = contenuto_precendente +"<li> Cognome utente: "+l+"</li>";
   }
-}
+} 
+
+
+/* soluzione alternativa bellissima trovata su internet da marcello e capita */
+
+// #1---Chiedo all'utente il suo cognome
+var askSurname = prompt('Inserisci il tuo Cognome');
+var maiuscolaAsk = askSurname.charAt(0).toUpperCase();
+var minuscolaAsk = askSurname.slice(1).toLowerCase();
+var result = maiuscolaAsk + minuscolaAsk;
+// #2---Creo il mio 'database cognomi'
+var listaCognomi = ['Rossi','Daje', 'Natali', 'Del Piero', 'Materazzi', 'Zidane', 'Corsini', 'Antonioni', 'Bergomi' ];
+// #3---Aggiungo tramite push il cognome inserito in input
+listaCognomi.push(result);
+listaCognomi.sort();
+var ordine = listaCognomi.indexOf(result) + 1;
+
+for (var i = 0; i < listaCognomi.length; i++) {
+    document.getElementById('clienti').innerHTML += '<br>' + listaCognomi[i];
+} 
+document.getElementById('posizione').innerHTML += 'La tua posizione è la numero:' + ordine;
+
+
+
